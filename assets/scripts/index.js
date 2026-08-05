ce082e799bfdc311caf7235866da0c974bc80a85
Maheswaran40 <eswermahes@gmail.com>
2026-08-05 10:58:15 +0530

    operators

---
+++ assets/scripts/index.js
@@ -0,0 +1,61 @@
+// console.log("welcome to js class 2")
+// console.warn("welcome to js class 2")
+// console.error("welcome to js class 2")
+
+// alert("battery is low")
+// confirm("battery is low")
+// prompt("enter your name")
+
+
+// variables
+// var let const 
+
+
+// | Feature    | `var`           | `let`           | `const`      |
+// | ---------- | --------------- | --------------- | ------------ |
+// | Scope      | Function        | Block           | Block        |
+// | Re-declare | ✅ Yes          | ❌ No           | ❌ No         |
+// | Re-assign  | ✅ Yes          | ✅ Yes          | ❌ No         |
+// | Hoisting   | Yes (undefined) | Yes (TDZ)       | Yes (TDZ)    |
+// | Common use | Old JS          | Changing values | Fixed values |
+
+
+// <!-- Data Types - String , Number , Boolean , Object ( Array , Object , Date , Null ) -->
+
+// console.log("welcome ",a);
+// const a=false
+let obj={name:"mahes" , age:23}
+let arr=["mahes",10,true,{name:"mani"}]
+
+// console.table(arr);
+
+// console.log(arr[3].name);
+
+// arithmetic operator  {+,-,*,/,%}
+
+// let a="20"
+// let b=10
+// console.log(a-b);
+
+
+// {> , >= , < , <= , == , != ,=== ,!==}
+
+// let a={name}
+// let b={name:"mahes"}
+// console.log(a==b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
